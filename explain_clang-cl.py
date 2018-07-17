0cef09052d809a6eaeb5acc58d19d997c0594b07
Ambiesoft <ambiesoft.trueff@gmail.com>
2018-07-17 09:38:58 +0900

    Add clang-cl

---
+++ explain_clang-cl.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from _ast import arg
+
+def printcmd(arg, explanation):
+    print('{}: {}'.format(arg,explanation))
+    
+    
+        
+def explain_clang_cl(commands):
+    print('clang-cl')
+    
+    i=0
+    count = len(commands)
+    while i < count:
+      arg = commands[i]
+      if False:
+        pass
+      elif arg in ['/nologo']:
+        printcmd(arg, '''
+  no logo
+        ''')
+        
+      i += 1  
+
+            
+        
+            
+        
+def printLine():
+    print('-' * 70)
+    
+if __name__ == '__main__':
+    explain_clang_cl(
+r'''/nologo /showIncludes  "-imsvcC:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.14.26428\ATLMFC\include" "-imsvcC:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.14.26428\include" "-imsvcC:\Program Files (x86)\Windows Kits\NETFXSDK\4.6.1\include\um" "-imsvcC:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\ucrt" "-imsvcC:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\shared" "-imsvcC:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\um" "-imsvcC:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\winrt" "-imsvcC:\Program Files (x86)\Windows Kits\10\include\10.0.17134.0\cppwinrt" -DV8_DEPRECATION_WARNINGS -DUSE_AURA=1 -DNO_TCMALLOC -DFULL_SAFE_BROWSING -DSAFE_BROWSING_CSD -DSAFE_BROWSING_DB_LOCAL -DCHROMIUM_BUILD -DFIELDTRIAL_TESTING_ENABLED "-DCR_CLANG_REVISION=\"336424-1\"" -D_HAS_NODISCARD -D_HAS_EXCEPTIONS=0 -DCOMPONENT_BUILD -D__STD_C -D_CRT_RAND_S -D_CRT_SECURE_NO_DEPRECATE -D_SCL_SECURE_NO_DEPRECATE -D_ATL_NO_OPENGL -D_WINDOWS -DCERT_CHAIN_PARA_HAS_EXTRA_FIELDS -DPSAPI_VERSION=1 -DWIN32 -D_SECURE_ATL -D_USING_V110_SDK71_ -DWINAPI_FAMILY=WINAPI_FAMILY_DESKTOP_APP -DWIN32_LEAN_AND_MEAN -DNOMINMAX -D_UNICODE -DUNICODE -DNTDDI_VERSION=0x0A000002 -D_WIN32_WINNT=0x0A00 -DWINVER=0x0A00 -DNDEBUG -DNVALGRIND -DDYNAMIC_ANNOTATIONS_ENABLED=0 -DUSING_V8_SHARED -DBLINK_CORE_IMPLEMENTATION=1 -DWEBP_EXTERN=extern -DUSE_EGL -DBLINK_IMPLEMENTATION=1 -DINSIDE_BLINK -DU_USING_ICU_NAMESPACE=0 -DU_ENABLE_DYLOAD=0 -DICU_UTIL_DATA_IMPL=ICU_UTIL_DATA_FILE -DUCHAR_TYPE=wchar_t -DGOOGLE_PROTOBUF_NO_RTTI -DGOOGLE_PROTOBUF_NO_STATIC_INITIALIZER -DPROTOBUF_USE_DLLS -DBORINGSSL_SHARED_LIBRARY -DSK_IGNORE_LINEONLY_AA_CONVEX_PATH_OPTS -DSK_HAS_PNG_LIBRARY -DSK_HAS_WEBP_LIBRARY -DSK_HAS_JPEG_LIBRARY -DSKIA_DLL -DGR_GL_IGNORE_ES3_MSAA=0 -DSKCMS_API=__declspec(dllexport) -DSK_SUPPORT_GPU=1 "-DSK_GPU_WORKAROUNDS_HEADER=\"gpu/config/gpu_driver_bug_workaround_autogen.h\"" -DGR_GL_FUNCTION_TYPE=__stdcall -D__STD_C -D_CRT_SECURE_NO_DEPRECATE -D_SCL_SECURE_NO_DEPRECATE -DWTF_USE_WEBAUDIO_FFMPEG=1 -DWTF_USE_DEFAULT_RENDER_THEME=1 -DUSE_LIBJPEG_TURBO=1 -DUSING_V8_SHARED -DWEBRTC_NON_STATIC_TRACE_EVENT_HANDLERS=0 -DGTEST_RELATIVE_PATH -DWEBRTC_CHROMIUM_BUILD -DWEBRTC_WIN -DABSL_ALLOCATOR_NOTHROW=1 -DNO_MAIN_THREAD_WRAPPING -DPNG_USE_DLL -DPNG_NO_MODULEDEF -DUSING_V8_SHARED -DLIBXSLT_STATIC -I. -I../.. -Igen -I../../third_party/libwebp/src -I../../third_party/wtl/include -I../../third_party/khronos -I../../gpu -I../../third_party/libyuv/include -I../../third_party/ced/src -I../../third_party/icu/source/common -I../../third_party/icu/source/i18n -I../../third_party/protobuf/src -I../../third_party/protobuf/src -Igen/protoc_out -I../../third_party/boringssl/src/include -I../../skia/config -I../../skia/ext -I../../third_party/skia/include/c -I../../third_party/skia/include/config -I../../third_party/skia/include/core -I../../third_party/skia/include/effects -I../../third_party/skia/include/encode -I../../third_party/skia/include/gpu -I../../third_party/skia/include/images -I../../third_party/skia/include/lazy -I../../third_party/skia/include/pathops -I../../third_party/skia/include/pdf -I../../third_party/skia/include/pipe -I../../third_party/skia/include/ports -I../../third_party/skia/include/utils -I../../third_party/skia/src/gpu -I../../third_party/skia/src/sksl -I../../third_party/angle/include -I../../third_party/angle/src/common/third_party/base -Igen/angle -I../../third_party/blink/renderer/platform/wtf/os-win32 -I../../third_party/libjpeg_turbo -I../../v8/include -Igen/v8/include -I../../third_party/webrtc_overrides -I../../third_party/webrtc -I../../third_party/iccjpeg -I../../third_party/libpng -I../../third_party/zlib -I../../third_party/ots/include -I../../v8/include -Igen/v8/include -I../../third_party/libxml/src/include -I../../third_party/libxml/win32/include -I../../third_party/libxslt/src -I../../third_party/snappy/src -I../../third_party/snappy/win32 /utf-8 /X -Wno-builtin-macro-redefined -D__DATE__= -D__TIME__= -D__TIMESTAMP__= -fcolor-diagnostics -fmerge-all-constants -Xclang -mllvm -Xclang -instcombine-lower-dbg-declare=0 -no-canonical-prefixes -fcomplete-member-pointers /Gy /FS /bigobj /d2FastFail /Zc:sizedDealloc- -fmsc-version=1911 -m32 /Brepro /W4 -Wimplicit-fallthrough -Wthread-safety /WX /wd4091 /wd4127 /wd4251 /wd4275 /wd4312 /wd4324 /wd4351 /wd4355 /wd4503 /wd4589 /wd4611 /wd4100 /wd4121 /wd4244 /wd4505 /wd4510 /wd4512 /wd4610 /wd4838 /wd4995 /wd4996 /wd4456 /wd4457 /wd4458 /wd4459 /wd4200 /wd4201 /wd4204 /wd4221 /wd4018 /wd4245 /wd4267 /wd4305 /wd4389 /wd4702 /wd4701 /wd4703 /wd4661 /wd4706 /wd4715 /wd4267 /wd4702 -Wno-missing-field-initializers -Wno-unused-parameter -Wno-c++11-narrowing -Wno-covered-switch-default -Wno-unneeded-internal-declaration -Wno-undefined-var-template -Wno-address-of-packed-member -Wno-nonportable-include-path -Wno-user-defined-warnings -Wno-unused-lambda-capture -Wno-null-pointer-arithmetic -Wno-enum-compare-switch -Wno-ignored-pragma-optimize /O1 /Ob2 /Oy- /Zc:inline /Gw /Oi /MD -Xclang -add-plugin -Xclang find-bad-constructs -Xclang -plugin-arg-find-bad-constructs -Xclang enforce-in-thirdparty-webkit -Xclang -plugin-arg-find-bad-constructs -Xclang check-enum-max-value -Wheader-hygiene -Wstring-conversion -Wtautological-overlap-compare /wd4305 /wd4324 /wd4714 /wd4800 /wd4996 -Xclang -add-plugin -Xclang blink-gc-plugin -Wglobal-constructors /FI../../third_party/blink/renderer/core/precompile_core.h -Wno-inconsistent-missing-override /wd4344 /wd4706 /wd4291 -imsvc ../../third_party/abseil-cpp -DLIBXML_STATIC= /Fpobj/third_party/blink/renderer/bindings/core/v8/bindings_core_impl_cc.pch /Yu../../third_party/blink/renderer/core/precompile_core.h /TP /wd4577 /GR- /c gen/third_party/blink/renderer/bindings/core/v8/bindings_core_impl_jumbo_4.cc /Foobj/third_party/blink/renderer/bindings/core/v8/bindings_core_impl/bindings_core_impl_jumbo_4.obj /Fd"obj/third_party/blink/renderer/bindings/core/v8/bindings_core_impl_cc.pdb"
+'''.split())
+    printLine()
--- explain_touch.py
+++ explain_touch.py
@@ -4,212 +4,29 @@ from _ast import arg
 def printcmd(arg, explanation):
     print('{}: {}'.format(arg,explanation))
     
-def explain_ls(commands):
-    ''' explain '''
+def explain_touch(commands):
+    ''' touch '''
     
-    print('ls: list files and directories')    
-    if commands[0] == 'ls':
+    print('touch - change file timestamps')    
+    if commands[0] == 'touch':
         commands.pop(0)
     
-    for arg in commands:
-        if arg in ['-a','--all']:
-            printcmd(arg,'do not ignore entries starting with .')
-        elif arg=='-A' or arg=='--almost-all':
-            printcmd(arg, 'do not list implied . and ..')
-        elif arg=='--author':
-            printcmd(arg, 'with -l, print the author of each file')
-        elif arg=='-b' or arg=='--escape':
-            printcmd(arg, 'print C-style escapes for nongraphic characters')
-        elif arg.startswith('--block-size='):
-            printcmd(arg, 
-                     '''scale   sizes   by   SIZE   before   printing    them.     E.g.,
-                     '--block-size=M'  prints sizes in units of 1,048,576 bytes.  See
-                     SIZE format below.''')
-        elif arg in ['-B', '--ignore-backups']:
-            printcmd(arg, 'do not list implied entries ending with ~')
-        elif arg=='-c':
-            printcmd(arg, '''with -lt: sort by, and show, ctime (time of last modification of
-              file  status  information)  with -l: show ctime and sort by name
-              otherwise: sort by ctime, newest first''')
-        elif arg=='-C':
-            printcmd(arg, 'list entries by columns')
-        elif arg.startswith('--color'):
-            printcmd(arg, '''colorize the output.   WHEN  defaults  to  'always'  or  can  be
-              'never' or 'auto'.  More info below''')
-        elif arg in ['-d','--directory']:
-            printcmd(arg, '''list  directory entries instead of contents, and do not derefer-
-                          ence symbolic links''')
-        elif arg in ['-D','--dired']:
-            printcmd(arg, '''generate output designed for Emacs' dired mode''')
-        elif arg=='-f':
-            printcmd(arg, 'do not sort, enable -aU, disable -ls --color')
-        elif arg in ['-F', '--classify']:
-            printcmd(arg, ' append indicator (one of */=>@|) to entries')
-        elif arg=='--file-type':
-            printcmd(arg, "likewise, except do not append '*'")
-        elif arg.startswith('--format'):
-            printcmd(arg, '''across -x, commas -m, horizontal -x, long -l, single-column  -1,
-              verbose -l, vertical -C''')
-        elif arg=='--full-time':
-            printcmd(arg, 'like -l --time-style=full-iso')
-        elif arg=='-g':
-            printcmd(arg, 'like -l, but do not list owner')
-        elif arg=='--group-directories-first':
-            printcmd(arg,'''group directories before files.
-
-              augment  with  a  --sort option, but any use of --sort=none (-U)
-              disables grouping''')
-        elif arg in ['-G', '--no-group']:
-            printcmd(arg,'''in a long listing, don't print group names''')
-        elif arg in ['-h', '--human-readable']:
-            printcmd(arg, '''with -l, print sizes in human readable format (e.g., 1K 234M 2G)''')
-        elif arg=='--si':
-            printcmd(arg, 'likewise (as -h), but use powers of 1000 not 1024')
-        elif arg in ['-H', '--dereference-command-line']:
-            printcmd(arg, 'follow symbolic links listed on the command line')
-        elif arg=='--dereference-command-line-symlink-to-dir':
-            printcmd(arg, """follow each command line symbolic link that points to  a  direc-
-                          tory""")
-        elif arg.startswith('--hide='):
-            printcmd(arg, ''' do  not  list implied entries matching shell PATTERN (overridden
-              by -a or -A)''')
-        elif arg.startswith('--indicator-style='):
-            printcmd(arg,'''append indicator with style WORD to entry names: none (default),
-              slash (-p), file-type (--file-type), classify (-F)''')
-        elif arg in ['-i', '--inode']:
-            printcmd(arg, 'print the index number of each file')
-        elif arg=='-I' or arg.startswith('--ignore='):
-            printcmd(arg, 'do not list implied entries matching shell PATTERN')
-        elif arg in ['-k', '--kibibytes']:
-            printcmd(arg, 'use 1024-byte blocks')
-        elif arg=='-l':
-            printcmd(arg, 'use a long listing format')
-        elif arg in ['-L', '--dereference']:
-            printcmd(arg, '''when showing file information for a symbolic link, show informa-
-                          tion for the file the link references rather than for  the  link
-              itself''')
-        elif arg=='-m':
-            printcmd(arg, 'fill width with a comma separated list of entries')
-        elif arg in ['-n', '--numeric-uid-gid']:
-            printcmd(arg, 'like -l, but list numeric user and group IDs')
-        elif arg in ['-N', '--literal']:
-            printcmd(arg, '''print  raw entry names (don't treat e.g. control characters spe-
-                                      cially)''')
-        elif arg=='-o':
-            printcmd(arg,'like -l, but do not list group information')
-        elif arg=='-p' or arg.startswith('--indicator-style='):
-            printcmd(arg,'append / indicator to directories')
-        elif arg in ['-q', '--hide-control-chars']:
-            printcmd(arg, 'print ? instead of non graphic characters')
-        elif arg=='--show-control-chars':
-            printcmd(arg,'''show non graphic characters as-is  (default  unless  program  is
-              'ls' and output is a terminal)''')
-        elif arg in ['-Q', '--quote-name']:
-            printcmd(arg, 'enclose entry names in double quotes')
-        elif arg.startswith('--quoting-style='):
-            printcmd(arg,'''use  quoting style WORD for entry names: literal, locale, shell,
-              shell-always, c, escape''')
-        elif arg in ['-r', '--reverse']:
-            printcmd(arg, 'reverse order while sorting')
-        elif arg in ['-R', '--recursive']:
-            printcmd(arg, 'list subdirectories recursively')
-        elif arg in ['-s','--size']:
-            printcmd(arg, 'print the allocated size of each file, in blocks')
-        elif arg=='-S':
-            printcmd(arg,'sort by file size')
-        elif arg.startswith('--sort='):
-            printcmd(arg,'''sort by WORD instead of name: none -U, extension  -X,  size  -S,
-              time -t, version -v''')
-        elif arg.startswith('--time='):
-            printcmd(arg,'''with  -l,  show time as WORD instead of modification time: atime
-              -u, access -u, use -u, ctime -c, or  status  -c;  use  specified
-              time as sort key if --sort=time''')
-        elif arg.startswith('--time-style='):
-            printcmd(arg,'''with  -l, show times using style STYLE: full-iso, long-iso, iso,
-              locale, +FORMAT.  FORMAT is interpreted like 'date';  if  FORMAT
-              is  FORMAT1<newline>FORMAT2, FORMAT1 applies to non-recent files
-              and FORMAT2 to recent files; if STYLE is prefixed with 'posix-',
-              STYLE takes effect only outside the POSIX locale''')
-        elif arg=='-t':
-            printcmd(arg,'sort by modification time, newest first')
-        elif arg=='-T' or arg.startswith('--tabsize='):
-            printcmd(arg,'assume tab stops at each COLS instead of 8')
-        elif arg=='-u':
-            printcmd(arg,'''with  -lt:  sort  by, and show, access time with -l: show access
-              time and sort by name otherwise: sort by access time''')
-        elif arg=='-U':
-            printcmd(arg,'do not sort; list entries in directory order')
-        elif arg=='-v':
-            printcmd(arg,'natural sort of (version) numbers within text')
-        elif arg=='-w' or arg.startswith('--width='):
-            printcmd(arg,'assume screen width instead of current value')
-        elif arg=='-x':
-            printcmd(arg,'list entries by lines instead of by columns')
-        elif arg=='-X':
-            printcmd(arg,'sort alphabetically by entry extension')
-        elif arg in ['-Z', '--context']:
-            printcmd(arg, 'print any SELinux security context of each file')
-        elif arg=='-1':
-            printcmd(arg,'list one file per line')
-        elif arg=='--help':
-            printcmd(arg,'display this help and exit')
-        elif arg=='--version':
-            printcmd(arg,'output version information and exit')
+    i = 0
+    count = len(commands)
+    while i < count:
+      arg = commands[i]
+      if False:
+        pass
+      elif arg in ['-a']:
+        printcmd(arg,'''
+  change only the access time
+        ''')
+      elif arg in ['-c', '--no-create']:
+        printcmd(arg,'''
+  do not create any files
+        ''')
             
-            
-            
-            
-            
-            
-            
-
-            
-            
-            
-  
-        
-             
-        # 'ls' can have options like '-lAF', which means same as '-l -A -F'.
-        elif arg.startswith('-'):
-            if len(arg)==2:
-                printcmd(arg, 'Unknown or too new or error')
-            else:
-                subarg = arg[1:]
-                # Separate and add '-'
-                message = 'same as "{}"'.format('-' +  ' -'.join(list(subarg)))
-                printcmd(arg, message)
-                
-        else:
-            message = 'list '
-            if arg=='.':
-                message += 'current directory'
-            elif arg=='..':
-                message += 'parent directory'
-            elif arg=='*':
-                message += '''every file and directory, in case of directory, list content
-                of the directory (not recursive unless '-R')'''
-            elif arg.startswith('*'):
-                message += '''every file and directory ends with '{}', in case of directory, list content
-                of the directory (not recursive unless '-R')'''.format(arg[1:])
-            elif arg.endswith('*'):
-                message += '''every file and directory starts with '{}', in case of directory, list content
-                of the directory (not recursive unless '-R')'''.format(arg[1:])
-            elif '*' in arg:
-                globArgs = arg.split('*')
-                globLen = len(globArgs)
-                if globLen==2:
-                    message += '''every file and directory starts with '{}' and ends with '{}', in case of directory, list content
-                    of the directory (not recursive unless '-R')'''.format(globArgs[0], globArgs[1])
-                else:
-                    message += '''every file and directory satisfies the glob, in case of directory, list content
-                    of the directory (not recursive unless '-R')'''
-            else:
-                message += arg
-                
-            printcmd(arg, message)
-            
-            
-            
+      i += 1  
             
         
             
@@ -218,15 +35,5 @@ def printLine():
     print('-' * 70)
     
 if __name__ == '__main__':
-    explain_ls('ls -Fta --block-size=100 -V aaa bbb .'.split())
+    explain_touch('touch -a -c'.split())
     printLine()
-    explain_ls('ls *'.split())
-    printLine()    
-    explain_ls('ls aaa*'.split())
-    printLine()    
-    explain_ls('ls *bbb'.split())
-    printLine()    
-    explain_ls('ls -Fta xxx*yyy'.split())
-    printLine()    
-    explain_ls('ls -Fta xxx*yyy*zzz'.split())
-    printLine()    
